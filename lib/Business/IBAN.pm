78a038b83dfba8acce6ffd7c6dcb5cd6b5bc09d4
Tina Müller <tinita@cpan.org>
2003-11-06 19:02:48 +0000

    Import of TINITA/Business-IBAN-0.05 from CPAN.
    gitpan-cpan-distribution: Business-IBAN
    gitpan-cpan-version:      0.05
    gitpan-cpan-path:         TINITA/Business-IBAN-0.05.tar.gz
    gitpan-cpan-author:       TINITA
    gitpan-cpan-maturity:     released

--- lib/Business/IBAN.pm
+++ lib/Business/IBAN.pm
@@ -3,16 +3,18 @@ package Business::IBAN;
 require 5.005_62;
 use Math::BigInt;
 use strict;
-use vars qw(@ISA @EXPORT_OK @EXPORT $VERSION @errors);
+use vars qw(@EXPORT_OK @EXPORT $VERSION @errors);
 
 require Exporter;
 
-@ISA = qw(Exporter);
+use base qw(Exporter);
 
 @EXPORT_OK = ( );
 
-@EXPORT = qw();
-$VERSION = '0.04';
+@EXPORT = ();
+$VERSION = '0.05';
+
+# error codes
 use constant IBAN_CTR => 0;
 use constant IBAN_BBAN => 1;
 use constant IBAN_ISO => 2;
@@ -103,13 +105,19 @@ sub valid {
 	my $self = shift;
 	my $ib = shift;
 	delete $self->{ERRORS};
-	$ib =~ tr/A-Za-z0-9//cd;
+	# remove spaces
+	$ib =~ tr/ //d;
+	# invalid characters
+	#(push @{$self->{ERRORS}}, IBAN_FORMAT2), return if $ib =~ tr/A-Za-z0-9//c;
 	$ib =~ s/^IBAN//i;
 	push @{$self->{ERRORS}}, IBAN_FORMAT unless $ib =~ m/^[A-Z][A-Z]/i;
 	return if $self->{ERRORS};
 	my $iso = substr($ib,0,2,"");
 	$iso =~ s/([A-Z])/(ord $1)-55/eg;
 	my $check = substr($ib,0,2,"");
+	# convert alpha characters to their ascii code
+	$ib =~ s/([A-Z])/(ord $1)-55/eg;
+	# iban still contains characters which are not numbers!
 	(push @{$self->{ERRORS}}, IBAN_FORMAT2), return if $ib =~ tr/0-9//c;
 	$ib .= "$iso$check";
 	$ib = Math::BigInt->new($ib);
@@ -119,11 +127,11 @@ sub valid {
 # --------------------------------------
 
 @errors = (
-	"No Country or Iso-Code",
-	"No BBAN (Bank-Number) or Bank Identifier and Accountnumber",
+	"No Country or Iso-Code supplied",
+	"No BBAN (Bank-Number) or Bank Identifier and Accountnumber supplied",
 	"Could not find country",
-	"IBAN must contain two-letter ISO-Code at the begining",
-	"IBAN must only contain numbers after the ISO-code",
+	"IBAN must contain two-letter ISO-Code at the beginning",
+	"IBAN must only contain only alphanumerics  after the ISO-code",
 	"IBAN is invalid",
 );
 
@@ -164,7 +172,8 @@ Business::IBAN - Validate and generate IBANs
     # codes see section ERROR-CODES
   }
   if ($iban->valid($ib)) {
-    print "$iban is valid\n";
+		# note: this also accepts IBANs in paper format with spaces added
+    print "$ib is valid\n";
   }
   else {
     $iban->printError();
@@ -173,7 +182,8 @@ Business::IBAN - Validate and generate IBANs
 =head1 DESCRIPTION
 
 With this module you can validate IBANs (International Bank
-Account Number) like "IBAN DE97123456781234567890".
+Account Number) like "IBAN DE97123456781234567890" (ISO 13616).
+(Note: spaces between numbers are allowed.)
 Note that this dos not (and cannot) assure that the bank
 account exists or that the bank account number for the
 bank itself is valid.
@@ -190,11 +200,13 @@ You can also create an IBAN if you supply
 - and either your country code (ISO3166)
   or the english name for your country.
 
+But note that only your bank is supposed to create your official IBAN.
+
 =head2 REQUIRES
 
-It requires the module Locale::Country, which you can get
-from www.cpan.org. It's a standard module since perl-version
-5.7.2.
+To get your country code the module Locale::Country is required, which
+you can get from www.cpan.org. It's a standard module since perl-version
+5.7.2. If you know your country code, you don't need the module.
 
 =head2 EXPORT
 
@@ -207,10 +219,12 @@ You can print your own error-messages. The array you get from
   my @errors = $iban->getError();
 are numbers which stand for the following errors:
 
-	0: No Country or Iso-Code
-	1: No BBAN (Bank-Number) or Bank Identifier and Accountnumber
+	0: No Country or Iso-Code supplied
+	1: No BBAN (Bank-Number) or Bank Identifier and Accountnumber supplied
 	2: Could not find country
-	3: IBAN must containt two-letter ISO-Code at the begining
+	3: IBAN must contain two-letter ISO-Code at the beginning
+	4: IBAN must only contain only alphanumerics after the ISO-code
+	5: IBAN is invalid
 
 =head2 CAVEATS
 
@@ -225,7 +239,7 @@ available for everyone. You can find informations about the IBAN at
 
 =head1 VERSION
 
-Business::IBAN Version 0.04, November 6th 2003
+Business::IBAN Version 0.05, November 6th 2003
 
 =head1 AUTHOR
 
--- t/test.t
+++ t/test.t
@@ -1,10 +1,11 @@
 
 # Before `make install' is performed this script should be runnable with
-# `make test'. After `make install' it should work as `perl test.pl'
-use lib "..";
-use Locale::Country;
-my $cc = country2code('Germany');
-BEGIN { $| = 1; print "1..3\n"; }
+# `make test'. After `make install' it should work as `perl t/test.t'
+use lib "lib";
+#use Locale::Country;
+#my $cc = country2code('Germany');
+my $cc = "DE";
+BEGIN { $| = 1; print "1..4\n"; }
 END {print "not ok 1\n" unless $loaded;}
 use Business::IBAN;
 $loaded = 1;
@@ -22,10 +23,20 @@ if ($ib) {
 else {
 	print "not ok 2\n";
 }
+#print "testing $ib\n";
 if ($iban->valid($ib)) {
 	print "ok 3\n";
 }
 else {
 	print "not ok 3\n";
+	#print $iban->printError();
+}
+$ib = "IBAN DE54360200410012345678"; # wrong
+if ($iban->valid($ib)) {
+	print "not ok 4\n";
+	#print $iban->printError();
+}
+else {
+	print "ok 4\n";
 }
 
